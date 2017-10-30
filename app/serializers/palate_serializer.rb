ad499c4b8f284e652dd80af1eedc1cfe06ba5416
joshlacey <williamjoshualacey@gmail.com>
2017-10-30 17:59:43 -0400

    heroku_backend

--- app/serializers/palate_serializer.rb
+++ app/serializers/palate_serializer.rb
@@ -6,25 +6,3 @@ class PalateSerializer < ActiveModel::Serializer
     User.find_by(id: object.user_id).username
   end
 end
-
-def swap(array, index1, index2)
-  temp = array[index1]
-  array[index1] = array[index2]
-  array[index2] = temp
-  return array
-end
-
-def selection_sort(array)
-  min = nil
-  mindex = nil
-
-  array.each.with_index do |e,i|
-    if min == nil
-      min = e
-      mindex = i
-    end
-    if min < e
-      
-    end
-  end
-end
